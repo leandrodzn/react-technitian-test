71d99ad68099474a273e2d6268137813d0f341f8
leandrodzn <leodzib100@gmail.com>
2024-12-28 21:51:21 -0600

    fetch cat fact and corresponding image based on the first word

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import { useState, useEffect } from 'react'
 
 const FACT_API_ENDPOINT = 'https://catfact.ninja/fact'
+const IMAGE_API_ENDPOINT = 'https://cataas.com/cat/says/:word?width=1000'
 
 export function App () {
-  const [fact, setFact] = useState('')
+  const [fact, setFact] = useState()
+  const [image, setImage] = useState()
 
   // Fetch a random cat fact from the API
   const fetchFact = async () => {
     try {
       const response = await fetch(FACT_API_ENDPOINT)
       const data = await response.json()
-      setFact(data.fact)
+
+      const { fact } = data
+      setFact(fact)
+      recoverFirstWord(fact)
     } catch (error) {
       setFact('')
     }
+  }
+
+  const recoverFirstWord = (fact) => {
+    const firstWord = fact.split(' ')[0]
+
+    const imageUrl = IMAGE_API_ENDPOINT.replace(':word', firstWord)
+    setImage(imageUrl)
   }
 
   // Fetch a cat fact on component mount
@@ -25,7 +37,9 @@ export function App () {
     <main>
       <h1>Cats App</h1>
       <span>Cat Fact:</span>
-      <p>{fact}</p>
+      {fact && <p>{fact}</p>}
+      <span>Image: </span>
+      {image && <img src={image} alt={`Image extracted using the first word from ${fact}`} />}
     </main>
   )
 }
