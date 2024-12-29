094457e91cb486256487fde8eeba19ea8d35199c
leandrodzn <leodzib100@gmail.com>
2024-12-28 21:05:59 -0600

    fetch random cat fact from API and display it in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+import { useState, useEffect } from 'react'
+
+const FACT_API_ENDPOINT = 'https://catfact.ninja/fact'
+
 export function App () {
-  return <h1>Cats App</h1>
+  const [fact, setFact] = useState('')
+
+  // Fetch a random cat fact from the API
+  const fetchFact = async () => {
+    const response = await fetch(FACT_API_ENDPOINT)
+    const data = await response.json()
+    setFact(data.fact)
+  }
+
+  // Fetch a cat fact on component mount
+  useEffect(() => {
+    fetchFact()
+  }, [])
+
+  return (
+    <main>
+      <h1>Cats App</h1>
+      <span>Cat Fact:</span>
+      <p>{fact}</p>
+    </main>
+  )
 }
